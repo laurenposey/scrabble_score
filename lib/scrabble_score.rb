9404404c03943a83ab47738aedda4abcc5c41399
Lauren Posey and Noah Yasskin <lauren@>
2016-06-28 10:45:43 -0700

    wrote a solution without inject because i ject is confusing

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,9 +1,22 @@
 class String
   define_method (:scrabble) do
-    letters = {"A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1, "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3, "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10, "Z" => 10}
+    scrabble_letters = {"A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1, "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3, "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10, "Z" => 10}
     self.upcase!()
-    self.split('').inject(0) do |sum, char|
-      sum += letters.fetch(char)
-    end
+    letters = self.split('')
+    score = 0
+      letters.each() do |letter|
+        score += scrabble_letters.fetch(letter)
+      end
+    score
   end
 end
+
+# class String
+#   define_method (:scrabble) do
+#     letters = {"A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1, "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3, "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10, "Z" => 10}
+#     self.upcase!()
+#     self.split('').inject(0) do |sum, char|
+#       sum += letters.fetch(char)
+#     end
+#   end
+# end
